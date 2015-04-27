122bac32131b90b46673c6895dada3f01018f52b
Sergey K <skozlovf@gmail.com>
2015-04-27 23:46:57 +0600

    Add 'nose' to required packages for testing.

--- setup.py
+++ setup.py
@@ -40,5 +40,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
+    tests_require=['nose>=1.0'],
     test_suite='nose.collector'
 )
