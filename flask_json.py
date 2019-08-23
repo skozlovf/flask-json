ad8dbb8b77e5d25e86d3dc15aaf28ec2420d3dfe
Sergey Kozlov <skozlovf@gmail.com>
2019-08-23 23:58:19 +0600

    Bump version to 0.3.4

--- flask_json.py
+++ flask_json.py
@@ -24,7 +24,7 @@ from werkzeug.exceptions import default_exceptions, BadRequest, HTTPException
 from flask import current_app, jsonify, request, Request, Response
 from flask import json
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 
 if sys.version_info[0] == 2:
