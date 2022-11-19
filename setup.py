9c88c5af9f6f3a46a77bb14de33283900dd0287d
Sergey Kozlov <skozlovf@gmail.com>
2022-11-19 15:39:04 +0600

    Replace travis with gh actions

--- setup.py
+++ setup.py
@@ -43,6 +43,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-cov']
 )
